9b9d5fc795ba55054c3fa0c41cee5140b8f60023
vyshali vijaywargi <vyshali4794@gmail.com>
2023-05-23 22:11:18 +0530

    ToDo list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,53 @@
 import React from 'react'
-import Navbar from './components/navbar.js'
-const App = () => {
-  return (
-    <Navbar/>
-  );
-}
+import {useState} from 'react';
+import ToDoList from './components/ToDoList';
+const App = ()=> {
+  const [inputList, setInputList]=useState("");
+  const[items,setItems]= useState([]);
 
-export default App
+  const itemEvent=(event)=>{
+    setInputList(event.target.value);
+  };
+  const listOfItems=()=> {
+  setItems((oldItems)=>
+  {
+    return[...oldItems,inputList];
+  }
+  );
+  setInputList("");
+  };
+  const deleteItem=(id)=> {
+    console.log("deleted");
+    setItems((oldItems)=>{
+      return oldItems.filter((arrElem,index)=>{
+return index !==id;
+      });
+    });
+  };
+  return (
+    <>
+    <div className="main-div">
+    <div className="center-div">
+    <br/>
+    <h1>ToDo List</h1>
+    <br/>
+    <input type="text" placeholder='Add a Item' value={inputList}onChange={itemEvent}/>
+    <button onClick={listOfItems}> + </button>
+
+    <ol> {
+      items.map((itemvalue, index)=> {
+      return <ToDoList key={index} id={index} 
+        text={itemvalue}
+        onSelect={deleteItem}
+      />;
+     } )
+    }
+    </ol>
+    </div>
+      
+    </div>
+    </>
+  );
+};
+export default App;
+
